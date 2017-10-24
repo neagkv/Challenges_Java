3f174251664aaac09056cd03d220a29df8bd40ad
Kevin Neag <neagkv@gmail.com>
2017-10-24 15:44:14 -0500

    Thread Example

---
+++ src/threadChallenge/ThreadController.java
@@ -0,0 +1,19 @@
+package src.threadChallenge;
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+public class ThreadController {
+
+    public static void main(String[] args) {
+
+        ThreadChallenge t1 = new ThreadChallenge("TONE");
+        ThreadChallenge t2 = new ThreadChallenge("TTWO");
+        ThreadChallenge t3 = new ThreadChallenge("TThree");
+        ThreadChallenge t4 = new ThreadChallenge("TFOUR");
+        ThreadChallenge t5 = new ThreadChallenge("TFIVE");
+        System.out.println("Complete");
+
+    }
+
+
+}
