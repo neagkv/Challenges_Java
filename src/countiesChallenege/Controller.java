dcea80fc35c4d07e325ad02a1f323ed26035bad5
Kevin Neag <neagkv@gmail.com>
2017-10-29 17:24:49 -0500

    county challegenege

--- src/countiesChallenege/Controller.java
+++ src/countiesChallenege/Controller.java
@@ -7,7 +7,7 @@ package src.countiesChallenege;
 
         1) which counties have the highest average weekly wage?
         2) which have the lowest weekly wage?
-        3) which counties have the highest and lowest total quarterly wages
+
         */
 
 import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
@@ -23,50 +23,173 @@ public class Controller {
 
     public static void main(String[] args) {
 
+        //array list object
         ArrayList<DemoInfo> list = new ArrayList<DemoInfo>();
 
+        //create a new object called file that is the path to your csv data
         File myFile = new File("/Users/kevinneag/Downloads/demographic___All Ages_.csv");
 
+        //created a string for each line of the file
         String line;
+        //a string array elements that holds each instance variables, even the two extra we created
         String[] elements;
+        //a string array  that holds the number of instance variables needed, which is 12
+        String[] elementsFinal = new String[12];
 
+        //create a buffered reader object that wraps a filereader object and takes it the file object created earlier
         try (BufferedReader br = new BufferedReader(new FileReader(myFile))) {
+            //skip the first line of the element
             br.readLine();
+            //when the br line object is not equal to zero
             while ((line = br.readLine()) != null) {
+
+
+                //seperate each row into coloumns by the comma
                 elements = line.split(",");
 
+                /*because some coloumns contain commas and others do not, created an if else statement to
+                that checks the number of rows and refits them into the elementsFinal array
+                 */
+
+                if (elements.length == 12) {
+                    for (int i = 0; i <= elements.length - 1; i++) {
+                        elementsFinal[i] = elements[i];
+                    }
+                }
+                if (elements.length == 14) {
+                    String a = elements[4].concat(",").concat(elements[5]);
+                    String b = elements[6].concat(",").concat(elements[7]);
+
+                    for (int i = 0; i <= elementsFinal.length - 1; i++) {
+                        if (i < 4) {
+                            elementsFinal[i] = elements[i];
+                        } else if (i == 4) {
+                            elementsFinal[i] = a;
+                        } else if (i == 5) {
+                            elementsFinal[i] = b;
+                        } else {
+                            elementsFinal[i] = elements[i + 2];
+                        }
+                    }
+                }
+                if (elements.length == 13) {
+                    String a = elements[4].concat(",").concat(elements[5]);
+
+                    for (int i = 0; i <= elementsFinal.length - 1; i++) {
+                        if (i < 4) {
+                            elementsFinal[i] = elements[i];
+                        } else if (i == 4) {
+                            elementsFinal[i] = a;
+                        } else {
+                            elementsFinal[i] = elements[i + 1];
+                        }
+                    }
+                }
+
+
+                //create an object of the DemoInfo class
                 DemoInfo di = new DemoInfo();
-                System.out.println(elements[0]);
-                System.out.println(elements[1]);
 
-                di.setArea_fips(Integer.parseInt(elements[0]));
-                di.setIndustry_code(Integer.parseInt(elements[1]));
-                di.setYear(Integer.parseInt(elements[2]));
-                di.setQrt(Integer.parseInt(elements[3]));
-                di.setArea_title(elements[4]);
-                di.setNdustry_title(elements[5]);
-                di.setQtrly_estabs_count(Integer.parseInt(elements[6]));
-                di.setMonth1_emplvl(Integer.parseInt(elements[7]));
-                di.setMonth2_emplvl(Integer.parseInt(elements[8]));
-                di.setMonth3_emplvl(Integer.parseInt(elements[9]));
-                di.setTotal_qtrly_wages(Long.parseLong(elements[10]));
-                di.setAvg_wkly_wage(Integer.parseInt(elements[11]));
+                    /* set each intsance variable of the di object as an element in the final array
+                    taking out the uneeded quotes that were in the csv file
+                     */
+                di.setArea_fips(Integer.parseInt(elementsFinal[0].replace("\"", "")));
+                di.setIndustry_code(Integer.parseInt(elementsFinal[1].replace("\"", "")));
+                di.setYear(Integer.parseInt(elementsFinal[2].replace("\"", "")));
+                di.setQrt(Integer.parseInt(elementsFinal[3].replace("\"", "")));
+                //this line is also split by a comma, which I created another row for
+                di.setArea_title(elementsFinal[4].replace("\"", ""));
+                di.setNdustry_title(elementsFinal[5].replace("\"", ""));
+                di.setQtrly_estabs_count(Integer.parseInt(elementsFinal[6].replace("\"", "")));
+                di.setMonth1_emplvl(Integer.parseInt(elementsFinal[7].replace("\"", "")));
+                di.setMonth2_emplvl(Integer.parseInt(elementsFinal[8].replace("\"", "")));
+                di.setMonth3_emplvl(Long.parseLong(elementsFinal[9].replace("\"", "")));
+                di.setTotal_qtrly_wages(Long.parseLong(elementsFinal[10].replace("\"", "")));
+                di.setAvg_wkly_wage(Integer.parseInt(elementsFinal[11].replace("\"", "")));
 
+                //add each di object to the array list
                 list.add(di);
-            }
 
-
-            for (DemoInfo x : list) {
-                System.out.println(x.toString());
 
             }
 
 
+//            //print out each element of each object in the array list
+//            for (int i = 0; i < list.size(); i ++) {
+//                DemoInfo reader = list.get(i);
+//                reader.giveInfo();
+//            }
+
+            //catch the io excpetion
         } catch (IOException x) {
             System.out.println("Exception Handled");
-            ;
+
         }
 
+        //create an object of the controller class to be able to call other methods from main
+        Controller hello = new Controller();
+        //
 
+         int max =hello.findMaxAvgWage(list);
+        String county = hello.findCountywithMAxWeeklyValue(list, max);
+        System.out.println("The county with the highest weekly wage is :" + " " + county);
+
+        String y = hello.findMinAvgWageObject(list).getArea_title();
+        System.out.println("The county with the lowest weekly wage is :" + " " + y);
+
+
+
+
+
+        System.out.println();
+        //String county = hello.findCountywithMAxWeeklyValue(list);
+
+
+    }
+
+
+    //new method that returns an object to find the lowest
+    public DemoInfo findMinAvgWageObject(ArrayList<DemoInfo> a) {
+        DemoInfo retObj = new DemoInfo();
+        for (int i = 0; i < a.size(); i++) {
+            DemoInfo b = a.get(i);
+            if (b.getAvg_wkly_wage() <= retObj.getAvg_wkly_wage()) {
+                retObj = b;
+            }
+        }
+        return retObj;
+    }
+
+
+    //new method to find the  max value for avegere weekly wage
+    public int findMaxAvgWage(ArrayList<DemoInfo> a) {
+
+        int max = 0;
+        //iterate through the array list to find the highest avgerage weekly wage
+        for (int i = 0; i < a.size(); i++) {
+            DemoInfo b = a.get(i);
+
+            max = b.getAvg_wkly_wage() > max ? b.getAvg_wkly_wage() : max;
+
+
+        }
+        return max;
+
+
+    }
+
+
+    //method to find the county associated with the greatest min wage
+    public String findCountywithMAxWeeklyValue(ArrayList<DemoInfo> a, int max) {
+
+        for (int i = 0; i < a.size(); i++) {
+            DemoInfo c = a.get(i);
+
+            if (c.getAvg_wkly_wage() == max) {
+                return c.getArea_title();
+            }
+
+        }
+        return "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
     }
 }
--- src/countiesChallenege/DemoInfo.java
+++ src/countiesChallenege/DemoInfo.java
@@ -1,28 +1,35 @@
 package src.countiesChallenege;
 
+//DemoInfo pojo
 public class DemoInfo {
-
+    //instance variables that each represent one coloumn in the csv file
     private int area_fips;
     private int industry_code;
     private int year;
     private int qrt;
     private String area_title;
+    //the coloumn for area_title contained a comma, so created an extra coloumn
+    private String area_title2;
+    //ndusty_title had an extra comma as well, created and extra coloumn
     private String ndustry_title;
+    private String ndustry_title2;
     private int qtrly_estabs_count;
     private int month1_emplvl;
    private int month2_emplvl;
-    private int month3_emplvl;
+    private long month3_emplvl;
     private long total_qtrly_wages;
     private int avg_wkly_wage;
 
-
-    public DemoInfo(int area_fips, int industry_code, int year, int qrt, String area_title, String ndustry_title, int qtrly_estabs_count, int month1_emplvl, int month2_emplvl, int month3_emplvl, long total_qtrly_wages, int avg_wkly_wage) {
+    //constructor
+    public DemoInfo(int area_fips, int industry_code, int year, int qrt, String area_title, String ndustry_title, int qtrly_estabs_count, int month1_emplvl, int month2_emplvl, long month3_emplvl, long total_qtrly_wages, int avg_wkly_wage) {
         this.area_fips = area_fips;
         this.industry_code = industry_code;
         this.year = year;
         this.qrt = qrt;
         this.area_title = area_title;
+        this.area_title = area_title;
         this.ndustry_title = ndustry_title;
+        this.ndustry_title = ndustry_title2;
         this.qtrly_estabs_count = qtrly_estabs_count;
         this.month1_emplvl = month1_emplvl;
         this.month2_emplvl = month2_emplvl;
@@ -31,9 +38,11 @@ public class DemoInfo {
         this.avg_wkly_wage = avg_wkly_wage;
     }
 
+    //empty constructor
     public DemoInfo() {
     }
 
+    //getters and setters
     public int getArea_fips() {
         return area_fips;
     }
@@ -106,11 +115,13 @@ public class DemoInfo {
         this.month2_emplvl = month2_emplvl;
     }
 
-    public int getMonth3_emplvl() {
+
+
+    public long getMonth3_emplvl() {
         return month3_emplvl;
     }
 
-    public void setMonth3_emplvl(int month3_emplvl) {
+    public void setMonth3_emplvl(long month3_emplvl) {
         this.month3_emplvl = month3_emplvl;
     }
 
@@ -129,4 +140,39 @@ public class DemoInfo {
     public void setAvg_wkly_wage(int avg_wkly_wage) {
         this.avg_wkly_wage = avg_wkly_wage;
     }
+
+    public String getNdustry_title2() {
+        return ndustry_title2;
+    }
+
+    public void setNdustry_title2(String ndustry_title2) {
+        this.ndustry_title2 = ndustry_title2;
+    }
+
+    public String getArea_title2() {
+        return area_title2;
+    }
+
+    public void setArea_title2(String area_title2) {
+        this.area_title2 = area_title2;
+    }
+
+
+// a method call to be able to call all instance variable except for the extra area_title and ndustry_title
+    public void giveInfo(){
+        System.out.print(getArea_fips() + " ");
+        System.out.print(getIndustry_code()+ " ");
+        System.out.print(getYear()+ " ");
+        System.out.print(getQrt()+ " ");
+        System.out.print(getArea_title()+ " ");
+        System.out.print(getNdustry_title()+ " ");
+        System.out.print(getQtrly_estabs_count()+ " ");
+        System.out.print(getMonth1_emplvl()+ " ");
+        System.out.print(getMonth2_emplvl()+ " ");
+        System.out.print(getMonth3_emplvl()+ " ");
+        System.out.print(getTotal_qtrly_wages()+ " ");
+        System.out.println(getAvg_wkly_wage());
+    }
+
+
 }
